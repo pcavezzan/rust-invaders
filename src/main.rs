eda4ddde6d7b246c6d7d81d2e974359696eb35f0
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-02 09:00:35 +0100

    Implementing win or lose

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw and render
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
@@ -84,6 +87,17 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(current_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // Win or Lose ?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'game_loop;
+        }
+
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'game_loop;
+        }
     }
 
     // Cleanup
